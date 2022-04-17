1936d3022ee005d58ad24ea0b2dc2a5d47dd0509
Marcus Gonçalves <nvrrox@gmail.com>
2022-04-17 00:40:49 -0300

    Create provider routes

--- routers/provider.py
+++ routers/provider.py
@@ -4,16 +4,31 @@ from utils import oauth
 
 router = APIRouter(
     prefix="/provider",
-    tags=["provider"],
+    tags=["Providers"],
     dependencies=[Depends(oauth.authentication)],
     responses={
         201: {"description": "Created"},
-        400: {"description": "Invalid body"}
+        400: {"description": "Invalid body"},
+        401: {"description": "Unauthorized"}
     }
 )
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
 async def create_provider(provider: Provider):
-    print(provider)
     return provider
+
+
+@router.get("/{company_name}", status_code=status.HTTP_200_OK)
+async def get_provider(company_name: str) -> dict:
+    return {"company_test": company_name}
+
+
+@router.put("/{company_name}", status_code=status.HTTP_202_ACCEPTED)
+async def update_provider(company_name: str, provider: Provider) -> dict:
+    return {"company_changed": company_name, "new_provider": provider}
+
+
+@router.delete("/{company_name}", status_code=status.HTTP_200_OK)
+async def delete_provider(company_name: str):
+    return {"company": company_name}
